bb30c3e6ce6a0194c06741b33854ea0740ed155c
Evgenii Bariev <bariev1233214554@gmail.com>
2026-05-14 19:26:16 +0700

    Update test.py
    Описание функций

--- PythonProject/test.py
+++ PythonProject/test.py
@@ -7,15 +7,19 @@ class TestCalculator(unittest.TestCase):
         self.calc = Calculator()
 
     def test_multiply_success(self):
+        """Тестированмие умнажения."""
         self.assertEqual(self.calc.multiply(2, 3), 6)
 
     def test_division_success(self):
+        """Тестированмие деления."""
         self.assertEqual(self.calc.division(6, 3), 2)
 
     def test_subtract_success(self):
+        """Тестированмие вычитания."""
         self.assertEqual(self.calc.subtraction(5, 3), 2)
 
     def test_add_success(self):
+        """Тестированмие сложения."""
         self.assertEqual(self.calc.adding(5, 3), 8)
 
     def tearDown(self):
